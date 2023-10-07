b019eec141284abc55ee4687eb16aea717ea5fc4
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2023-10-07 14:05:51 +0200

    Complete Menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+const menuContainer = document.querySelectorAll(".menu-container");
 
+const imgFood = [
+	"ramen_yakitori",
+	"ramen_tantamen",
+	"ramen_yakiniku",
+	"ramen_kotteri",
+	"",
+];
+let imgNumber = 0;
+const bgcImgAdd = () => {
+	menuContainer.forEach(container => {
+		container.style.backgroundImage = `url(../img/Food/${imgFood[imgNumber]}.jpg)`;
+		imgNumber++;
+	});
+};
+
+bgcImgAdd();
